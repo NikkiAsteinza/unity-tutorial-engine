b02c72dd42c077bb6a1585f7037e0474d64947b6
Nikki <nikki.asteinza@gmail.com>
2024-03-13 16:07:41 +0100

    Fix missing namespace

--- Editor/DrawIfPropertyDrawer.cs
+++ Editor/DrawIfPropertyDrawer.cs
@@ -1,71 +1,74 @@
 using UnityEditor;
 using UnityEngine;
 
-[CustomPropertyDrawer(typeof(DrawIfAttribute))]
-public class DrawIfPropertyDrawer : PropertyDrawer
+namespace TutorialEngine
 {
-    #region Fields
-
-    // Reference to the attribute on the property.
-    DrawIfAttribute drawIf;
-
-    // Field that is being compared.
-    SerializedProperty comparedField;
-
-    #endregion
-
-    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    [CustomPropertyDrawer(typeof(DrawIfAttribute))]
+    public class DrawIfPropertyDrawer : PropertyDrawer
     {
-        if (!ShowMe(property) && drawIf.disablingType == DrawIfAttribute.DisablingType.DontDraw)
-            return 0f;
+        #region Fields
 
-        // The height of the property should be defaulted to the default height.
-        return base.GetPropertyHeight(property, label);
-    }
+        // Reference to the attribute on the property.
+        DrawIfAttribute drawIf;
 
-    /// <summary>
-    /// Errors default to showing the property.
-    /// </summary>
-    private bool ShowMe(SerializedProperty property)
-    {
-        drawIf = attribute as DrawIfAttribute;
-        // Replace propertyname to the value from the parameter
-        string path = property.propertyPath.Contains(".") ? System.IO.Path.ChangeExtension(property.propertyPath, drawIf.comparedPropertyName) : drawIf.comparedPropertyName;
+        // Field that is being compared.
+        SerializedProperty comparedField;
 
-        comparedField = property.serializedObject.FindProperty(path);
+        #endregion
 
-        if (comparedField == null)
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            Debug.LogError("Cannot find property with name: " + path);
-            return true;
+            if (!ShowMe(property) && drawIf.disablingType == DrawIfAttribute.DisablingType.DontDraw)
+                return 0f;
+
+            // The height of the property should be defaulted to the default height.
+            return base.GetPropertyHeight(property, label);
         }
 
-        // get the value & compare based on types
-        switch (comparedField.type)
-        { // Possible extend cases to support your own type
-            case "bool":
-                return comparedField.boolValue.Equals(drawIf.comparedValue);
-            case "Enum":
-                return comparedField.enumValueIndex.Equals((int)drawIf.comparedValue);
-            default:
-                Debug.LogError("Error: " + comparedField.type + " is not supported of " + path);
+        /// <summary>
+        /// Errors default to showing the property.
+        /// </summary>
+        private bool ShowMe(SerializedProperty property)
+        {
+            drawIf = attribute as DrawIfAttribute;
+            // Replace propertyname to the value from the parameter
+            string path = property.propertyPath.Contains(".") ? System.IO.Path.ChangeExtension(property.propertyPath, drawIf.comparedPropertyName) : drawIf.comparedPropertyName;
+
+            comparedField = property.serializedObject.FindProperty(path);
+
+            if (comparedField == null)
+            {
+                Debug.LogError("Cannot find property with name: " + path);
                 return true;
-        }
-    }
+            }
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        // If the condition is met, simply draw the field.
-        if (ShowMe(property))
-        {
-            EditorGUI.PropertyField(position, property);
-        } //...check if the disabling type is read only. If it is, draw it disabled
-        else if (drawIf.disablingType == DrawIfAttribute.DisablingType.ReadOnly)
-        {
-            GUI.enabled = false;
-            EditorGUI.PropertyField(position, property);
-            GUI.enabled = true;
+            // get the value & compare based on types
+            switch (comparedField.type)
+            { // Possible extend cases to support your own type
+                case "bool":
+                    return comparedField.boolValue.Equals(drawIf.comparedValue);
+                case "Enum":
+                    return comparedField.enumValueIndex.Equals((int)drawIf.comparedValue);
+                default:
+                    Debug.LogError("Error: " + comparedField.type + " is not supported of " + path);
+                    return true;
+            }
         }
-    }
 
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // If the condition is met, simply draw the field.
+            if (ShowMe(property))
+            {
+                EditorGUI.PropertyField(position, property);
+            } //...check if the disabling type is read only. If it is, draw it disabled
+            else if (drawIf.disablingType == DrawIfAttribute.DisablingType.ReadOnly)
+            {
+                GUI.enabled = false;
+                EditorGUI.PropertyField(position, property);
+                GUI.enabled = true;
+            }
+        }
+
+    }
 }
